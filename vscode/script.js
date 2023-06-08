fa36377fd86a1c6e997f6c9aa63915587ab469f6
Manu-Amaral <134888880+Manu-Amaral@users.noreply.github.com>
2023-06-08 09:59:00 -0300

    :tada:

---
+++ vscode/script.js
@@ -0,0 +1,15 @@
+function toggleMode() {
+    const html = document.documentElement
+
+   html.classList.toggle("ligth")
+
+const img = document.querySelector("#profile img")
+
+if(html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/manudeoculos.png")
+} else {
+
+  img.setAttribute("src", "./assets/avatar.png")
+}
+
+}
