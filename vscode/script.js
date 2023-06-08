8fc8c7eb29875c172fffd7d5d05996d3629f3551
Manu-Amaral <134888880+Manu-Amaral@users.noreply.github.com>
2023-06-08 17:19:27 -0300

    ajust container width

--- vscode/script.js
+++ vscode/script.js
@@ -1,14 +1,16 @@
 function toggleMode() {
     const html = document.documentElement
-
    html.classList.toggle("ligth")
 
+// pegar a tag img
 const img = document.querySelector("#profile img")
 
+//substituir a imagem
 if(html.classList.contains("light")) {
+  // se estiver light mode, adicionar a imagem light
     img.setAttribute("src", "./assets/manudeoculos.png")
 } else {
-
+// se tiver sem light mode, manter a imagem normal
   img.setAttribute("src", "./assets/avatar.png")
 }
 
